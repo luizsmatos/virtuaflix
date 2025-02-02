3d8239fbcfee5a8d430a99f0ee3c14baa358f6dd
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-02 18:37:40 -0300

    feat(app.module.ts): add VideoModule and PrismaService to AppModule for enhanced functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
+import { VideoModule } from './modules/videos/video.module';
+import { PrismaService } from './prisma.service';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [VideoModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
