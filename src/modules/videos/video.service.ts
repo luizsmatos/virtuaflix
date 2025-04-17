fb0b35397f8e575c1baa1cb69053348e2e6fbc2b
Luiz Matos <gusstavopdm@gmail.com>
2025-04-17 09:59:27 -0300

    feat(video.service.ts): implement video upload functionality with UUID and input validation to enhance video management capabilities

--- src/modules/videos/video.service.ts
+++ src/modules/videos/video.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@src/prisma.service';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class VideoService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  uploadFile(files: Record<string, Express.Multer.File[]>) {
-    console.log(files);
-    return {
-      message: 'Video uploaded successfully',
+  async uploadFile(input: Input) {
+    const newVideo = {
+      id: randomUUID(),
+      title: input.title,
+      description: input.description,
+      url: input.videoUrl,
+      thumbnailUrl: input.thumbnailUrl,
+      sizeInKb: input.sizeInKb,
+      duration: input.duration || 100,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     };
+
+    return this.prismaService.video.create({
+      data: newVideo,
+    });
   }
+}
+
+interface Input {
+  title: string;
+  description: string;
+  videoUrl: string;
+  thumbnailUrl: string;
+  sizeInKb: number;
+  duration?: number;
 }
